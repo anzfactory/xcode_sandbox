b85caf33acb356455e91ad8cc11c1f64dad2fed6
anzfactory <anz.factory@gmail.com>
2021-04-30 10:47:11 +0900

    first commit

---
+++ x-sandbox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  x-sandbox
+//
+//  Created by sasato on 2021/04/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
