da8b693485b4b5d05b387ae14eb6cd4b29b68301
anzfactory <anz.factory@gmail.com>
2021-06-03 23:05:47 +0900

    Use BackgroundTask

--- x-sandbox/AppDelegate.swift
+++ x-sandbox/AppDelegate.swift
@@ -5,15 +5,30 @@
 //  Created by sasato on 2021/04/30.
 //
 
+import BackgroundTasks
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    var title: String?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: "xyz.anzfactory.x-sandbox.bgtask", using: nil) { task in
+            let que = OperationQueue()
+            que.maxConcurrentOperationCount = 1
+            let operation = TestTaskOperation()
+            operation.completionBlock = {
+                task.setTaskCompleted(success: true)
+            }
+            que.addOperation(operation)
+        }
+        
+        BGTaskScheduler.shared.getPendingTaskRequests { reqs in
+            reqs.forEach { print($0.identifier) }
+        }
+        
+        BGTaskScheduler.shared.cancelAllTaskRequests()
         return true
     }
 
@@ -31,6 +46,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
 }
 
+extension AppDelegate {
+    func sceduleBackgroundTask() {
+        let taskRequest = BGAppRefreshTaskRequest(identifier: "xyz.anzfactory.x-sandbox.bgtask")
+        taskRequest.earliestBeginDate = .init(timeIntervalSinceNow: 5 * 60) // 5分後以降
+        do {
+            try BGTaskScheduler.shared.submit(taskRequest)
+        } catch {
+            print(error)
+        }
+    }
+}
--- x-sandbox/ViewController.swift
+++ x-sandbox/ViewController.swift
@@ -5,15 +5,48 @@
 //  Created by sasato on 2021/04/30.
 //
 
+import AppTrackingTransparency
+import BackgroundTasks
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.text = "Background Tasks"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        BGTaskScheduler.shared.getPendingTaskRequests { reqs in
+            guard !reqs.isEmpty else {
+                return
+            }
+            DispatchQueue.main.async {
+                self.label.text = reqs.map { $0.identifier }.joined(separator: ",")
+            }
+        }
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
 }
 
+final class TestTaskOperation: Operation {
+    override func main() {
+        print("start background task")
+        ATTrackingManager.requestTrackingAuthorization { status in
+            print(status)
+            UserDefaults.standard.setValue("\(status.rawValue)", forKey: "title")
+        }
+    }
+}
