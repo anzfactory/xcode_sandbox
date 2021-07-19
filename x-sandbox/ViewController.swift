8ffbddcd7f1d84befcb3ccd42472342a89ea42fe
anzfactory <anz.factory@gmail.com>
2021-07-19 15:58:00 +0900

    StatusBarStyle変更のあれこれ検証用

--- x-sandbox/ViewController.swift
+++ x-sandbox/ViewController.swift
@@ -7,13 +7,80 @@
 
 import UIKit
 
+class NavigationController: UINavigationController {
+    override var childForStatusBarStyle: UIViewController? {
+        visibleViewController
+    }
+}
+
 class ViewController: UIViewController {
+    
+    private let button: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("画面遷移", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        .lightContent
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        navigationController?.modalPresentationCapturesStatusBarAppearance = true
+        view.backgroundColor = .black
+        view.addSubview(button)
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        button.addTarget(self, action: #selector(type(of: self).tappedButton(sender:)), for: .touchUpInside)
     }
 
-
+    @objc private func tappedButton(sender: UIButton) {
+        present(ChildViewController(), animated: true, completion: nil)
+    }
 }
 
+class ChildViewController: UIViewController {
+    
+    private let button: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("戻る", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        .lightContent
+    }
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        modalTransitionStyle = .crossDissolve
+        modalPresentationStyle = .overFullScreen
+        modalPresentationStyle = .overFullScreen
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red.withAlphaComponent(0.4)
+        view.addSubview(button)
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        button.addTarget(self, action: #selector(type(of: self).tappedButton(sender:)), for: .touchUpInside)
+    }
+    
+    @objc private func tappedButton(sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
